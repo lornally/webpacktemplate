7cd3828623766b1d5c5715be18f1ac31e7e4a970
bergman <machangkun1984@qq.com>
2020-11-12 12:31:01 +0800

    lgo

--- base/src/index.js
+++ base/src/index.js
@@ -1,12 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './2020.jpg';
 
-  function component() {
-    const element = document.createElement('div');
 
-   // lodash，现在通过一个 script 引入
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+function component() {
+	const element=document.createElement('div');
 
-    return element;
-  }
+	// lodash，现在通过一个 script 引入
+	element.innerHTML=_.join(['Hello','webpack'],' ');
+	element.classList.add('hello');
+	// 将图像添加到我们已经存在的 div 中。
+	const myIcon=new Image();
+	myIcon.src=Icon;
 
-  document.body.appendChild(component());
+	element.appendChild(myIcon);
+	return element;
+}
+
+document.body.appendChild(component());
