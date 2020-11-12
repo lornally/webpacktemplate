8ba6c389fe08f20de90071817720182c160145f4
bergman <machangkun1984@qq.com>
2020-11-13 01:22:56 +0800

    lgo

--- base/src/index.js
+++ base/src/index.js
@@ -1,13 +1,33 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './2020.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+//预缓存, 未来需要的内容
+//生成的html: <link rel="prefetch" href="login-modal-chunk.js">
+//import(/* webpackPrefetch: true */ 'lodash');
+
+//预加载, 本页需要的内容
+//生成html: <link rel="preload">
+//import(/* webpackPreload: true */ 'lodash');
+
+
+// 没有警告
+import jsond from './data.json';
+
+// 显示警告，规范不允许这样做。但是可以这样做.
+import {foo} from './data.json';
 
 
 function component() {
 	const element=document.createElement('div');
 
 	// lodash，现在通过一个 script 引入
-	element.innerHTML=_.join(['Hello','webpack'],' ');
+	element.innerHTML=_.join(['Hello','webpack',Data,Notes,jsond,foo,toml,yaml,json],' ');
 	element.classList.add('hello');
 	// 将图像添加到我们已经存在的 div 中。
 	const myIcon=new Image();
@@ -18,3 +38,39 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+
+
+function getComponent() {
+
+	return import(/* webpackPreload: true */ 'lodash').then(({default: _}) => {
+		const element=document.createElement('div');
+
+		element.innerHTML=_.join(['Hello','promise webpack'],' ');
+
+		return element;
+
+	}).catch(error => 'An error occurred while loading the component');
+}
+
+
+
+//async 写法
+async function agetComponent() {
+
+	const element=document.createElement('div');
+	const {default: _}=await import(/* webpackPrefetch: true */ 'lodash');
+
+	element.innerHTML=_.join(['Hello','async webpack'],' ');
+
+	return element;
+}
+
+(async () => {
+	const compo=await agetComponent();
+	document.body.appendChild(compo);
+
+	getComponent().then(component => {
+		document.body.appendChild(component);
+	})
+})();
--- base/webpack.config.js
+++ base/webpack.config.js
@@ -1,37 +1,137 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path=require('path');
+const {CleanWebpackPlugin}=require('clean-webpack-plugin');
+const toml=require('toml');
+const yaml=require('yamljs');
+const json5=require('json5');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+const Manifest=require('webpack-manifest-plugin');
+
+module.exports={
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+	},
+
+	entry: {
+		index: {import: './src/index.js',dependOn: 'shared'},
+		another: {import: './src/another-module.js',dependOn: 'shared'},
+		shared: 'lodash',
+	//	sty: './src/style.css'
+	},
+	plugins: [
+		new CleanWebpackPlugin({cleanStaleWebpackAssets: false}), //这样就不会清除index.html
+
+		new HtmlWebpackPlugin({
+			title: '管理输出',
+		}),
+			new Manifest(),
 
 
-  module.exports = {
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+	],
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname,'dist'),
+		publicPath: '/',
+
+	},
+
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
 		},
-		plugins: [
-			new CleanWebpackPlugin(),
-		],
-   module: {
-     rules: [
-       {
-         test: /\.css$/,
-         use: [
-           'style-loader',
-           'css-loader',
-         ],
-			 },
-			 {
+		runtimeChunk: 'single',
+
+	},
+
+
+
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {//特别注意presets: [[这里两个中括号
+						presets: [['@babel/preset-env',{ 
+							targets: {
+								//"browsers": "chrome >= 86",
+								"chrome": "86",
+								"node": "15"
+							} 
+						}]],
+						plugins: ['@babel/plugin-transform-runtime','@babel/plugin-proposal-object-rest-spread']					}
+				}
+			},
+			{
+				test: /\.css$/,
+				exclude: /(node_modules|bower_components)/,
+
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+			{
 				test: /\.(png|svg|jpg|gif)$/,
+				exclude: /(node_modules|bower_components)/,
+
 				use: [
 					'file-loader',
 				],
 			},
 			{
 				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				exclude: /(node_modules|bower_components)/,
+
 				use: [
 					'file-loader',
 				],
 			},
-     ],
-   },
-  };
+			{
+				test: /\.(csv|tsv)$/,
+				exclude: /(node_modules|bower_components)/,
+
+				use: [
+					'csv-loader',
+				],
+			},
+			{
+				test: /\.xml$/,
+				exclude: /(node_modules|bower_components)/,
+
+				use: [
+					'xml-loader',
+				],
+			},
+			{
+				test: /\.toml$/,
+				exclude: /(node_modules|bower_components)/,
+
+				type: 'json',
+				parser: {
+					parse: toml.parse
+				}
+			},
+			{
+				test: /\.yaml$/,
+				exclude: /(node_modules|bower_components)/,
+
+				type: 'json',
+				parser: {
+					parse: yaml.parse
+				}
+			},
+			{
+				test: /\.json5$/,
+				exclude: /(node_modules|bower_components)/,
+
+				type: 'json',
+				parser: {
+					parse: json5.parse
+				}
+			}
+		],
+	},
+};
